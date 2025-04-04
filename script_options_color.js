20dfffffcbea2da29fe04db75d774ee977aebf68
AkameMasami <satomasamiyui@gmail.com>
2025-04-04 12:49:28 -0300

    *remade viewer info

--- script_options_color.js
+++ script_options_color.js
@@ -3,29 +3,24 @@ const textCar = document.querySelector("#scroll-based")
 function colorChanger(color) {
     const colorText = document.getElementById("color-text");
 
-    if (!gradientBox || !colorText) {
-        console.error("Elemento não encontrado!");
-        return;
-    }
-
-    if (color === "white") {
-        textCar.style.color = "black"
+    if (color === "white") { // se o botão branco for clicado
+        textCar.style.color = "#fff"
 
         colorText.textContent = "Branco Metálico Estelar";
-        gradientBox.style.backgroundImage = "linear-gradient(to bottom,#ebeae6,#6d6c6a,#232324)";
+        gradientBox.style.backgroundImage = "linear-gradient(to bottom,#1f1f1f 1%,#ebeae6,#6d6c6a,#232324,#1f1f1f)";
     } 
     
     else if (color === "black") {
         textCar.style.color = "#fff"
 
         colorText.textContent = "Preto Metálico Infinito";
-        gradientBox.style.backgroundImage = "linear-gradient(to bottom, #1b1b1b, #141414,black )";
+        gradientBox.style.backgroundImage = "linear-gradient(to bottom,#1f1f1f 1%, #1b1b1b, #141414, black, #1f1f1f )";
     }
     else if (color === "wine") {
-        textCar.style.color = "#1c1c1c"
+        textCar.style.color = "#fff"
 
         colorText.textContent = "Vermelho Metálico Vinho";
-        gradientBox.style.backgroundImage = "linear-gradient(to bottom, #a51830, #501819, black)";
+        gradientBox.style.backgroundImage = "linear-gradient(to bottom,#1f1f1f 1%, #a51830, #501819, black, #1f1f1f)";
     }
    
 }
